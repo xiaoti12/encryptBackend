fdf9ba5d9aaaf9f211a9d199bc4a010426a17da3
ljy <ljy@ljy.com>
2023-06-25 16:05:08 +0800

    add mapper

---
+++ src/main/java/com/networkflow/backendspringboot3/mapper/TLSFlowMapper.java
@@ -0,0 +1,9 @@
+package com.networkflow.backendspringboot3.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.networkflow.backendspringboot3.model.domain.UEFlow;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TLSFlowMapper extends BaseMapper<TLSFlow> {
+}
